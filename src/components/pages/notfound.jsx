184eec66f02555066d8ff5c972ed40e5ba4e6c7e
Kenneth Adjei-mensah Adjetey <kenadjei20@gmail.com>
2023-11-20 15:51:37 +0000

    additional pages needed for slider

---
+++ src/components/pages/notfound.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import '../notfound.css'
+
+export default function NotFound  ()  {
+  return (
+    <div className="not-found">
+      <h1>404 - Not Found</h1>
+      <p>Sorry, the page you are looking for does not exist.</p>
+      {/* You can add additional content or styling here. */}
+    </div>
+  );
+};
